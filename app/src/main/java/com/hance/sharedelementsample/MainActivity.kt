02bebad919387f38038f8f4cbeb75c1d904b0659
Kim Hyun Su <jwzz9324@tukorea.ac.kr>
2024-11-27 00:13:20 +0900

    background content

--- app/src/main/java/com/hance/sharedelementsample/MainActivity.kt
+++ app/src/main/java/com/hance/sharedelementsample/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hance.sharedelementsample.ui.theme.SharedElementSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +29,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             SharedElementSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            verticalArrangement = Arrangement.spacedBy(10.dp)
+                        ) {
+                            items(30) {
+                                Greeting(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(50.dp)
+                                        .background(Color(0xFF00BCD4), RoundedCornerShape(0.2f)),
+                                    name = "baby"
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
